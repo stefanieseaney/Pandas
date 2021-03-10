b066454db505c5b5898cac51fc033a12d26385c7
Stefanie Seaney <stefanie.seaney@gmail.com>
2021-03-10 14:15:45 -0600

    Working with series.

---
+++ pandas_series.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+grades = pd.Series([87, 100, 94])
+
+print(grades)
+
+same_grade = pd.Series(98.6, range(3))
+
+print(same_grade)
+
+print(grades[0])
+
+grades.count()
+grades.mean()
+grades.min()
+grades.max()
+grades.std()
+
+#  includes all of the methods above
+print(grades.describe())
+
+grades = pd.Series([87, 100, 94], indexes=["Wally", "Eva", "Sam"])
+
+print(grades)
+
+# if you initialize a series with a dictionary, its keys become indexes and values become series values
+grades = pd.Series({"Wally": 87, "Eva": 100, "Sam": 94})
+
+print(grades["Eva"])
+
+print(grades.Wally)
+
+print(grades.dtype)
+
+print(grades.values)
